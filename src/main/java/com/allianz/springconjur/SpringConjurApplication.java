39b642dca6006fabe874129389e33fdd60a788d9
Leonardo Ginting <onesLeo@users.noreply.github.com>
2019-11-21 15:26:49 +0700

    initialization commit
    first commit of the project

---
+++ src/main/java/com/allianz/springconjur/SpringConjurApplication.java
@@ -0,0 +1,13 @@
+package com.allianz.springconjur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConjurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConjurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allianz/springconjur/SpringConjurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianz.springconjur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConjurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
